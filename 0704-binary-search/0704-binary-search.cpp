c31a00877d3749c67000e7c157bee6c2038eb3d4
Bhavya Vaish <bhavya.16419@gmail.com>
2026-07-31 09:14:51 +0530

    Time: 0 ms (100%), Space: 31.1 MB (99.24%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -6,19 +6,18 @@ public:
         while(low<=high)
         {
             int mid=low+(high-low)/2;
-
-            if(nums[mid]==target)
-            {
+             if(nums[mid]==target)
+             {
                 return mid;
-            }
-            else if(nums[mid]>target)
-            {
+             }
+             else if(nums[mid]>target)
+             {
                 high=mid-1;
-            }
-            else
-            {
+             }
+             else
+             {
                 low=mid+1;
-            }
+             }
         }
         return -1;
     }
