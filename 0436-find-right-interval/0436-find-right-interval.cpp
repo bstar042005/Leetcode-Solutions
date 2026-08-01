7bde46779073a4039b7a03433a4f6fe8cc1ff24d
Bhavya Vaish <bhavya.16419@gmail.com>
2026-08-01 14:57:25 +0530

    Time: 8 ms (67.27%), Space: 29.6 MB (58.64%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
-        int n = intervals.size();
+        int n=intervals.size();
 
-        vector<pair<int, int>> starts;
-        for (int i = 0; i < n; i++) {
-            starts.push_back({intervals[i][0], i});
+        vector<pair<int,int>> starts;
+        for(int i=0;i<n;i++)
+        {
+            starts.push_back({intervals[i][0],i});
         }
 
-        sort(starts.begin(), starts.end());
+        sort(starts.begin(),starts.end());
 
-        vector<int> ans(n, -1);
+        vector <int> ans(n,-1);
 
-        for (int i = 0; i < n; i++) {
-            int end = intervals[i][1];
+        for(int i=0;i<n;i++)
+        {
+            int idx=-1;
+            int low=0;
+            int high=n-1;
+            int end=intervals[i][1];
 
-            int low = 0, high = n - 1;
-            int idx = -1;
+            while(low<=high)
+            {
+                int mid=low+(high-low)/2;
 
-            while (low <= high) {
-                int mid = low + (high - low) / 2;
-
-                if (starts[mid].first >= end) {
-                    idx = starts[mid].second;
-                    high = mid - 1;   
-                } else {
-                    low = mid + 1;
+                if(starts[mid].first>=end)
+                {
+                    idx=starts[mid].second;
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
                 }
             }
-
-            ans[i] = idx;
+            ans[i]=idx;
         }
-
         return ans;
     }
 };
