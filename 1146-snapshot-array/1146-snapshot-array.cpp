66f89c7b397cf1b70a1250c18d93c3f23848fdfc
Bhavya Vaish <bhavya.16419@gmail.com>
2026-08-01 14:59:24 +0530

    Time: 64 ms (78.7%), Space: 177.3 MB (81.99%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.cpp
@@ -0,0 +1,59 @@
+class SnapshotArray {
+public:
+
+    vector<vector<pair<int,int>>> arr;
+    int snapId;
+
+    SnapshotArray(int length) {
+
+        arr.resize(length);
+        snapId = 0;
+
+    }
+
+    void set(int index, int val) {
+
+        if(!arr[index].empty() && arr[index].back().first == snapId)
+        {
+            arr[index].back().second = val;
+        }
+        else
+        {
+            arr[index].push_back({snapId,val});
+        }
+
+    }
+
+    int snap() {
+
+        return snapId++;
+
+    }
+
+    int get(int index, int snap_id) {
+
+        vector<pair<int,int>> &vec = arr[index];
+
+        int low = 0;
+        int high = vec.size()-1;
+
+        int ans = 0;
+
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+
+            if(vec[mid].first<=snap_id)
+            {
+                ans = vec[mid].second;
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+
+        return ans;
+    }
+};
