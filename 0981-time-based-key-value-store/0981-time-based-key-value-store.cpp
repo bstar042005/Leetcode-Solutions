f813c89d77ed535db75714df071bc3c7af8173e3
Bhavya Vaish <bhavya.16419@gmail.com>
2026-08-03 10:27:12 +0530

    Time: 48 ms (66.02%), Space: 137.1 MB (46.24%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -1,45 +1,43 @@
 class TimeMap {
 public:
+    unordered_map <string,vector<pair<int,string>>> mp;
 
-    unordered_map<string, vector<pair<int,string>>> mp;
-
-    TimeMap() {
+    TimeMap()
+    {
 
     }
 
-    void set(string key, string value, int timestamp) {
-
+    void set(string key, string value, int timestamp)
+    {
         mp[key].push_back({timestamp,value});
-
     }
 
-    string get(string key, int timestamp) {
-
+    string get(string key, int timestamp)
+    {
         if(mp.find(key)==mp.end())
             return "";
 
-        vector<pair<int,string>> &vec = mp[key];
+        vector<pair<int,string>> &vec= mp[key];
 
-        int low = 0;
-        int high = vec.size()-1;
-
-        string ans = "";
+        int low=0;
+        int high=vec.size()-1;
+        string ans="";
 
         while(low<=high)
         {
-            int mid = low + (high-low)/2;
+            int mid=low+(high-low)/2;
+
 
             if(vec[mid].first<=timestamp)
             {
-                ans = vec[mid].second;
-                low = mid+1;
+                ans=vec[mid].second;
+                low=mid+1;
             }
             else
             {
-                high = mid-1;
+                high=mid-1;
             }
         }
-
         return ans;
     }
 };
